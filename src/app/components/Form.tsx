5ba71f53d02c9bbf999e2b41efc33597e4fd96dd
virgilebouali <virgile.bouali@gmail.com>
2024-02-08 18:46:18 +0100

    cn5

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -70,7 +70,7 @@ export const Form = () => {
                 className="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]" required/>
             <input type='text' placeholder='Subject'
                 className="w-full rounded-md py-2.5 px-4 border text-sm outline-[#007bff]" required/>
-            <textarea placeholder='Message' rows="6"
+            <textarea placeholder='Message' rows={6}
                 className="w-full rounded-md px-4 border text-sm pt-2.5 outline-[#007bff] required"></textarea>
             <button type='button'
                 className="text-white bg-[#007bff] hover:bg-blue-600 font-semibold rounded-md text-sm px-4 py-2.5 w-full">Send</button>
