c2dd50b0c3d0805807de5626c34a1031e47642d2
virgilebouali <virgile.bouali@gmail.com>
2024-03-05 15:30:53 +0100

    ok

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -38,7 +38,7 @@ export const Hero = () => {
 			<div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
 			<Link href="#contact" className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
 <span className="w-48 h-48 rounded rotate-[-40deg] bg-blue absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0 text-blue"></span>
-<span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white" h>Contactez moi 📪</span>
+<span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">Contactez moi 📪</span>
 </Link>			</div>
 		</div>
 	</div>
