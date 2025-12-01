b20c4059b5dc2041782e75421dce3784c54f3a20
Virgile Bouali <94724413+virgilebouali@users.noreply.github.com>
2025-12-01 18:53:17 +0100

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
+  images: {
       remotePatterns: [
-        { protocol: 'https', hostname: 'cdn.rareblocks.xyz' },
-        { protocol: 'https', hostname: 'assets.website-files.com' }
+          { protocol: 'https', hostname: 'cdn.rareblocks.xyz' },
+          { protocol: 'https', hostname: 'assets.website-files.com' }
       ]
-    },
-  };
+  },
   
-  export default nextConfig;
-  
+  // NOUVELLE SECTION DE REDIRECTIONS
+  async redirects() {
+      return [
+          {
+              source: '/',
+              // Redirige l'URL racine (https://www.marthas-office.com/) 
+              // vers votre langue par défaut
+              destination: '/fr', 
+              permanent: true, // Utilise le code d'état 308 (Permanent Redirect)
+          },
+      ];
+  },
+  // FIN DE LA NOUVELLE SECTION
+};
+
+export default nextConfig;
